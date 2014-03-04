ed123bad83e41e18a31cde2ffabf34964d209302
Dunya Kirkali <dunyakirkali@gmail.com>
2014-03-04 23:19:30 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trainz::Application.config.secret_key_base = '37eee9d4633c9365edf4b76de158494ee4c8e96ae573ef14bba768cc49068bfe33c8580c2cf4870d4b286cea1e7510bef7905e2dac3b9c1c315e54dd73523f7f'
